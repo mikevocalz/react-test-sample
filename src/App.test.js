e101fdb306f04fa555e6aa1b3b88f26f72c3b3c1
Mike Allen <mikeallen@Mikes-MacBook-Pro.local>
2018-05-15 16:32:02 -0400

    Add testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, configure } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe("<App />", () => {
+  it("renders 1 <App /> component", () => {
+    const component = shallow(<App />);
+    expect(component).toHaveLength(1);
+  })
 });
